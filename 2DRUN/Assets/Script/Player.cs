599d4b1f7f301edcd6889d800feff853f2a80d83
shane95146 <66512908+shane95146@users.noreply.github.com>
2020-07-04 21:00:33 +0800

    類別、成員與事件區域

--- 2DRUN/Assets/Script/Player.cs
+++ 2DRUN/Assets/Script/Player.cs
@@ -51,8 +51,79 @@ public class Player : MonoBehaviour
 
 
 
-        #endregion
+    #endregion
 
+    #region 方法區域
+    // C#括號符號都是成對出現的:() [] {} "" ''
+    // 摘要: 方法的說明
+    // 在方法上方添加三條/
+    //自訂方法 - 不會執行的，需要呼叫
+    //API - 功能倉庫
+
+
+    /// <summary>
+    /// 角色的跳躍功能:跳躍動畫、撥放音效與往上跳
+    /// </summary>
+    private void Jump()
+    {
+        print("跳躍");
 
     }
+
+    /// <summary>
+    /// 角色的滑行功能:滑行動畫、播放音效、縮小碰撞範圍
+    /// </summary>
+    private void Slide()
+    {
+        print("滑行");
+    }
+
+    /// <summary>
+    /// 碰到障礙物時受傷:扣血
+    /// </summary>
+    private void Hit()
+    {
+
+    }
+
+    /// <summary>
+    /// 吃金幣:金幣數量增加，更新介面，金幣特效
+    /// </summary>
+    private void EatCoin()
+    {
+       
+    }
+
+    /// <summary>
+    /// 角色死亡:動畫、遊戲結束
+    /// </summary>
+    private void Dead()
+    {
+
+    }
+
+    #endregion
+
+    #region 事件區域
+    // 開始 start
+    //開始遊戲時執行一次
+    //初始化:
+
+    private void Start()
+    {
+        // 呼叫跳躍方法
+        Jump();
+    }
+
+
+    // 更新 Update
+    //撥放遊戲後一秒執行約60次。60FPS
+    //移動、監聽玩家鍵盤、滑鼠與觸控
+    private void Update()
+    {
+        Slide();
+    
+    }
+    #endregion
+}
 
